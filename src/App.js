d5a51496a675d1203429c2e86073416364d96479
Jesús <jesusalxen@gmail.com>
2020-03-15 13:09:17 +0100

    Fix url error

--- src/App.js
+++ src/App.js
@@ -9,39 +9,39 @@ function App() {
      */
     {
       name: "A Coruña",
-      src: "http://www.crtvg.es/camweb/cam.php?c=coruna"
+      src: "//www.crtvg.es/camweb/cam.php?c=coruna"
     },
     {
       name: "A Coruña movil",
-      src: "http://www.crtvg.es/camweb/cam.php?c=mcoruna"
+      src: "//www.crtvg.es/camweb/cam.php?c=mcoruna"
     },
     {
       name: "Bando, San Marcos",
-      src: "http://www.crtvg.es/camweb/cam.php?c=bando"
+      src: "//www.crtvg.es/camweb/cam.php?c=bando"
     },
     {
       name: "Cabo Fisterra",
-      src: "http://www.crtvg.es/camweb/cam.php?c=fisterra"
+      src: "//www.crtvg.es/camweb/cam.php?c=fisterra"
     },
     {
       name: "Ferrol",
-      src: "http://www.crtvg.es/camweb/cam.php?c=ferrol"
+      src: "//www.crtvg.es/camweb/cam.php?c=ferrol"
     },
     {
       name: "Monte Pedroso",
-      src: "http://www.crtvg.es/camweb/cam.php?c=pedroso"
+      src: "//www.crtvg.es/camweb/cam.php?c=pedroso"
     },
     {
       name: "Ortigueira",
-      src: "http://www.crtvg.es/camweb/cam.php?c=ortigueira"
+      src: "//www.crtvg.es/camweb/cam.php?c=ortigueira"
     },
     {
       name: "Praza das Praterías",
-      src: "http://www.crtvg.es/camweb/cam.php?c=praterias"
+      src: "//www.crtvg.es/camweb/cam.php?c=praterias"
     },
     {
       name: "Praza do Obradoiro",
-      src: "http://www.crtvg.es/camweb/cam.php?c=obradoiro"
+      src: "//www.crtvg.es/camweb/cam.php?c=obradoiro"
     },
 
     /**
@@ -49,15 +49,15 @@ function App() {
      */
     {
       name: "Lugo",
-      src: "http://www.crtvg.es/camweb/cam.php?c=lugo"
+      src: "//www.crtvg.es/camweb/cam.php?c=lugo"
     },
     {
       name: "O Cebreiro",
-      src: "http://www.crtvg.es/camweb/cam.php?c=cebreiro"
+      src: "//www.crtvg.es/camweb/cam.php?c=cebreiro"
     },
     {
       name: "Portomarín",
-      src: "http://www.crtvg.es/camweb/cam.php?c=portomarin"
+      src: "//www.crtvg.es/camweb/cam.php?c=portomarin"
     },
 
     /**
@@ -65,78 +65,78 @@ function App() {
      */
     {
       name: "A Rúa",
-      src: "http://www.crtvg.es/camweb/cam.php?c=arua"
+      src: "//www.crtvg.es/camweb/cam.php?c=arua"
     },
     {
       name: "Cabeza de Manzaneda",
-      src: "http://www.crtvg.es/camweb/cam.php?c=manzaneda"
+      src: "//www.crtvg.es/camweb/cam.php?c=manzaneda"
     },
     {
       name: "Cabeza de Manzaneda - Estación 1",
-      src: "http://www.crtvg.es/camweb/cam.php?c=oca_manzaneda_1"
+      src: "//www.crtvg.es/camweb/cam.php?c=oca_manzaneda_1"
     },
     {
       name: "Cabeza de Manzaneda - Estación 2",
-      src: "http://www.crtvg.es/camweb/cam.php?c=oca_manzaneda_2"
+      src: "//www.crtvg.es/camweb/cam.php?c=oca_manzaneda_2"
     },
     {
       name: "Cabeza de Manzaneda - Estación 3",
-      src: "http://www.crtvg.es/camweb/cam.php?c=oca_manzaneda_3"
+      src: "//www.crtvg.es/camweb/cam.php?c=oca_manzaneda_3"
     },
     {
       name: "Celanova",
-      src: "http://www.crtvg.es/camweb/cam.php?c=celanova"
+      src: "//www.crtvg.es/camweb/cam.php?c=celanova"
     },
     {
       name: "Ourense (móbil)",
-      src: "http://www.crtvg.es/camweb/cam.php?c=mourense"
+      src: "//www.crtvg.es/camweb/cam.php?c=mourense"
     },
     {
       name: "Ourense fixa",
-      src: "http://www.crtvg.es/camweb/cam.php?c=ourense"
+      src: "//www.crtvg.es/camweb/cam.php?c=ourense"
     },
     {
       name: "Verín",
-      src: "http://www.crtvg.es/camweb/cam.php?c=verin"
+      src: "//www.crtvg.es/camweb/cam.php?c=verin"
     },
     /**
      * Pontevedra
      */
     {
       name: "A Guarda",
-      src: "http://www.crtvg.es/camweb/cam.php?c=aguarda"
+      src: "//www.crtvg.es/camweb/cam.php?c=aguarda"
     },
     {
       name: "Baiona",
-      src: "http://www.crtvg.es/camweb/cam.php?c=baiona"
+      src: "//www.crtvg.es/camweb/cam.php?c=baiona"
     },
     {
       name: "Lalín",
-      src: "http://www.crtvg.es/camweb/cam.php?c=lalin"
+      src: "//www.crtvg.es/camweb/cam.php?c=lalin"
     },
     {
       name: "O Grove",
-      src: "http://www.crtvg.es/camweb/cam.php?c=ogrove"
+      src: "//www.crtvg.es/camweb/cam.php?c=ogrove"
     },
     {
       name: "Pontevedra",
-      src: "http://www.crtvg.es/camweb/cam.php?c=pontevedra"
+      src: "//www.crtvg.es/camweb/cam.php?c=pontevedra"
     },
     {
       name: "Portonovo",
-      src: "http://www.crtvg.es/camweb/cam.php?c=portonovo"
+      src: "//www.crtvg.es/camweb/cam.php?c=portonovo"
     },
     {
       name: "Sanxenxo",
-      src: "http://www.crtvg.es/camweb/cam.php?c=sanxenxo"
+      src: "//www.crtvg.es/camweb/cam.php?c=sanxenxo"
     },
     {
       name: "Tui",
-      src: "http://www.crtvg.es/camweb/cam.php?c=tui"
+      src: "//www.crtvg.es/camweb/cam.php?c=tui"
     },
     {
       name: "Vigo (móbil)",
-      src: "http://www.crtvg.es/camweb/cam.php?c=mvigo"
+      src: "//www.crtvg.es/camweb/cam.php?c=mvigo"
     }
   ];
 
